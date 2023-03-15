ace0b69e265ccca0b58bb1567be3606b4c760ddb
HEEKDragonOne <1425138134@qq.com>
2023-03-15 14:48:32 +0800

    1

---
+++ ant-demo/src/main/java/com/tothefor/antdemo/AntController.java
@@ -0,0 +1,13 @@
+package com.tothefor.antdemo;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author DragonOne
+ * @Date 2023/3/15 14:47
+ * @墨水记忆 www.tothefor.com
+ */
+@RestController
+public class AntController {
+
+}
